f63565f0a6ed1c6d1b581fa04b566375ec4b71d1
pauloProgg <alemida.desenvolve@gmail.com>
2024-04-08 15:14:09 -0300

    fix: remove comentarios

--- database/factories/CardapioFactory.php
+++ database/factories/CardapioFactory.php
@@ -22,15 +22,4 @@ class CardapioFactory extends Factory
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        /* return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]); */
-    }
 }
